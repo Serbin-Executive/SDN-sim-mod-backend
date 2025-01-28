f628c2d0d7a88933ee77fab04e9b3e4bde3060d2
Ivan Rybin <rybin1van@outlook.com>
2025-01-28 22:16:45 +0300

    [B-1] Create http request ws connection

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,25 @@ import express, { type Express } from "express";
 import DefaultErrorHandler from "./middlewares/DefaultErrorHandler";
 import { webSocketCreateConnection } from "./controllers/WebSocketController";
 import { createModels, startModels, stopModels } from "./controllers/ModelsController";
+import { PORT } from "./utils/constants";
+import CORS from "./middlewares/CORS";
+import { checkUrl } from "./controllers/CheckUrlController";
 
 const app: Express = express();
-const port = process.env.PORT || 5500;
+// const port = process.env.PORT || 5500;
 
 app.use(DefaultErrorHandler);
+app.use(CORS);
 
 createModels();
 
 webSocketCreateConnection();
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+app.get(`/check-url/:url`, (req, res) => {
+    const { url } = req.params;
+
+    checkUrl(url, res) });
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
